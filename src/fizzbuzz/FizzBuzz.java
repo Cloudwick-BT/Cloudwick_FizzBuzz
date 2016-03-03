de696bbf96c7f206e0fadd3f05c1b1a28b9371d0
Bhavin Tandel <Bhavin Tandel>
2016-03-03 14:46:02 +0000

    User input using Buffered reader

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,13 +1,14 @@
 package fizzbuzz;
 import java.util.*;
-
+import java.io.*;
 public class FizzBuzz {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		int num;
-		Scanner inputScan = new Scanner(System.in);
+		//Scanner inputScan = new Scanner(System.in);
 		System.out.println("Enter number to be checked : ");
-		num = inputScan.nextInt();
-		
+		//num = inputScan.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		num = Integer.parseInt(br.readLine());
 		if(num%3 != 0 && num%5 != 0){
 			System.out.println(num);				
 		}
