5fab904d790a686b08607abe6d499b181ac703ee
Bhavin Tandel <Bhavin Tandel>
2016-03-04 12:21:24 +0000

    Features added to check negative number

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,19 +1,27 @@
 package fizzbuzz;
 import java.util.*;
+
+
 public class FizzBuzz {
 	public static void main(String[] args) {
 		int num=0;
-		System.out.println("Enter number to be checked : ");
+		System.out.println("Enter number (greater than 0) to be checked : ");
 		Scanner inputScan = new Scanner(System.in);
 
 		try {
 			num = inputScan.nextInt();
+			if (num <=0){
+				System.out.print("Number should be positive and greater than 0");
+				return;
+			}
 		}
 		catch (InputMismatchException e){
 			System.out.print("Input need to be an integer");
 		}
+		finally {
+			inputScan.close();
+		}
 		
-		inputScan.close();
 		int temp = num;
 		/* iteration */
 		for(num = 1; num<=temp; num++){
